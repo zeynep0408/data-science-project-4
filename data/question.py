3ce8ee46b22b1efc6f30ea0179640439d187a484
zeynep0408 <tzeynepceren@gmail.com>
2026-02-28 17:09:38 +0300

    completed

--- data/question.py
+++ data/question.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 ## Bu değeri localinde çalışırken kendi passwordün yap. Ama kodu pushlarken 'postgres' olarak bırak.
-password = 'postgres'
+password = '3453'
 
 def connect_db():
     return psycopg2.connect(
@@ -15,31 +15,51 @@ def connect_db():
 def create_view_completed_orders():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""create or replace view  completed_orders as
+                            select * from pj.orders 
+                            where status='completed'""")
             conn.commit()
 
 def create_view_electronics_products():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""create or replace view electronics as 
+                            select product_name from pj.products
+                            where category='Electronics'""")
             conn.commit()
 
 def total_spending_per_customer():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""with toplam_harcama as 
+                        (select c.full_name,sum(price)  from pj.customers as c  
+                        join pj.orders as o on 
+                        o.customer_id=c.customer_id
+                        join pj.products as p on
+                        p.product_id=o.product_id
+                        group by c.full_name)
+	                        select * from toplam_harcama""")
             return cur.fetchall()
 
 def order_details_with_total():
     with connect_db() as conn:
         with conn.cursor() as cur:
-            cur.execute("""""")
+            cur.execute("""
+                    WITH order_details AS (select (p.price * o.quantity) AS total_price
+                    FROM pj.orders o
+                    JOIN pj.products p ON o.product_id = p.product_id)
+                        SELECT * FROM order_details;""")
             return cur.fetchall()
 
 def get_customer_who_bought_most_expensive_product():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT c.full_name
+                FROM pj.customers as c
+                JOIN pj.orders as o ON c.customer_id = o.customer_id
+                JOIN pj.products as p ON o.product_id = p.product_id
+                ORDER BY p.price DESC
+                LIMIT 1;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -49,7 +69,13 @@ def get_customer_who_bought_most_expensive_product():
 def get_order_status_descriptions():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT order_id,status,
+                    CASE 
+                    WHEN status = 'completed' THEN 'Tamamlandı'
+                    WHEN status = 'cancelled' THEN 'İptal Edildi'
+                        ELSE 'Bilinmiyor' 
+                    END AS status_description
+                FROM pj.orders;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -59,7 +85,9 @@ def get_order_status_descriptions():
 def get_products_above_average_price():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT product_name, price
+                    FROM pj.products
+                    WHERE price > (SELECT AVG(price) FROM pj.products);""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -69,7 +97,15 @@ def get_products_above_average_price():
 def get_customer_categories():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT c.full_name,
+                    CASE 
+                    WHEN COUNT(o.order_id) > 5 THEN 'Sadık Müşteri'
+                    WHEN COUNT(o.order_id) BETWEEN 2 AND 5 THEN 'Orta Seviye'
+                    ELSE 'Yeni Müşteri'
+                END AS customer_category
+                FROM pj.customers as  c
+                    JOIN pj.orders as o ON c.customer_id = o.customer_id
+                    GROUP BY c.customer_id, c.full_name;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -79,7 +115,10 @@ def get_customer_categories():
 def get_recent_customers():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT c.full_name
+                    FROM pj.customers c
+                    JOIN pj.orders o ON c.customer_id = o.customer_id
+                    WHERE o.order_date >= '2023-05-20'::date - 30;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -89,7 +128,12 @@ def get_recent_customers():
 def get_most_ordered_product():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT p.product_name, SUM(o.quantity) AS toplam_adet
+                    FROM pj.products p
+                    JOIN pj.orders o ON p.product_id = o.product_id
+                    GROUP BY p.product_name
+                    ORDER BY toplam_adet DESC
+                    LIMIT 1;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -99,7 +143,13 @@ def get_most_ordered_product():
 def get_product_price_categories():
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("""""")
+    cur.execute("""SELECT product_name,price,
+                    CASE
+                        WHEN price > 1000 THEN 'Pahalı'
+                        WHEN price BETWEEN 100 AND 500 THEN 'Orta'
+                        ELSE 'Ucuz'
+                    END AS price_category
+                    FROM pj.products;""")
     result = cur.fetchall()
     cur.close()
     conn.close()
--- tests/test_question.py
+++ tests/test_question.py
@@ -121,7 +121,7 @@ def run_tests():
     
     url = "https://edugen-backend-487d2168bc6c.herokuapp.com/projectLog/"
     payload = {
-        "user_id": 34,
+        "user_id": 403,
         "project_id": 37,
         "user_score": round(user_score, 2),
         "is_auto": False
